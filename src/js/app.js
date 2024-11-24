5d9b6ad9393337a748b9d1b5ad933b415bbca14f
alexgithub <alexgithub@yandex.ru>
2024-11-24 12:33:53 +1000

    commit-1.1

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,27 @@
-import './view/field';
-import './view/title';
-import './rules/rules';
+function createDiv() {
+  let divPole = document.createElement("div");
+
+  divPole.classList = "pole-game";
+
+  document.querySelector("body").append(divPole);
+
+  for (let i = 0; i < 16; i++) {
+    let div = document.createElement("div");
+    div.classList = "pole";
+    document.querySelector(".pole-game").append(div);
+  }
+}
+
+createDiv();
+
+let arr = document.querySelectorAll(".pole");
+function intervalGoblin() {
+  for (let i = 0; i < arr.length; i++) {
+    arr[i].classList = "pole";
+  }
+
+  let random = Math.floor(Math.random() * (15 - 0 + 1)) + 0;
+  arr[random].classList = "pole goblinPole";
+}
+
+setInterval(intervalGoblin, 1500);
